12f2703895e715b00abe503454f84cf5bfe43908
HarrisonShort <work.harrisonshort@gmail.com>
2022-08-03 15:52:02 +1000

    CHANGE to a class component for UserDetailsComponent

--- client/src/UserDetailsComponent.js
+++ client/src/UserDetailsComponent.js
@@ -1,28 +1,29 @@
-import React from "react";
+import { React, Component } from "react";
 import './App.css';
 
-function UserDetailsComponent() {
-    const [data, setData] = React.useState(null);
+export default class UserDetailsComponent extends Component {
 
-    React.useEffect(() => {
+    componentDidMount() {
         fetch("http://localhost:3000/spotify-user-details")
             .then((response) => response.json())
             .then((data) => {
-                setData(data);
-                console.log(data);
+                this.setState(data, () => console.log(this.state));
             });
-    }, []);
+    }
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Logged in as {data.display_name}
-                </p>
-                <img src={data.images[0].url}></img>
-            </header>
-        </div>
-    );
+    render() {
+        if (!this.state) {
+            return <div></div>
+        }
+        return (
+            < div className="App" >
+                <header className="App-header">
+                    <p>
+                        Logged in as {this.state.display_name}
+                    </p>
+                    <img src={this.state.images[0].url}></img>
+                </header>
+            </div >
+        );
+    }
 }
-
-export default UserDetailsComponent;

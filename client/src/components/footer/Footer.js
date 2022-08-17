cb86a975ff17589acdb8605c1e5833bd088f5dae
HarrisonShort <work.harrisonshort@gmail.com>
2022-08-17 10:01:08 +1000

    ADD footer to SpotifyDashboard and HomeComponent

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -4,10 +4,10 @@ export default function Footer() {
     return (
         <div className="footer-container">
             <p className="footer-copyright">
-                <a href="https://harrisonshort.com">Harrison Short</a> (c) 2022
+                <a href="https://harrisonshort.com" target="_blank">Harrison Short</a> (c) 2022
             </p>
             <p>
-                <a href="https://ko-fi.com/harrishun">
+                <a href="https://ko-fi.com/harrishun" target="_blank">
                     <img src={require('./images/kofi_button_dark.png')} alt="kofi-button" />
                 </a>
             </p>

c1c3a66a36d5cb19b6c690023305f0a5fbd32f4c
HarrisonShort <work.harrisonshort@gmail.com>
2022-08-31 18:28:12 +1000

    ADD add to playlist flow/tasks to spotifyUtils, plus "dummy" functions

--- client/src/components/spotify/spotifyUtils.js
+++ client/src/components/spotify/spotifyUtils.js
@@ -59,3 +59,39 @@ export function getTopTracks(spotifyApi) {
             console.log(err);
         });
 }
+
+// Get all user playlists when logging in - DONE
+// Get the Hottest 100 Helper playlist if it exists
+// Implement adding to playlist
+// If playlist doesn't exist, create playlist
+// Implement removing from the playlist 
+
+export function addTrackToPlaylist(spotifyApi, playlist, track) {
+    spotifyApi.addTracksToPlaylist('5ieJqeLJjjI8iJWaxeBLuK', [track])
+        .then(function (data) {
+            console.log('Added tracks to playlist!');
+        }, function (err) {
+            console.log('Something went wrong!', err);
+        });
+}
+
+export function removeTrackFromPlaylist(spotifyApi) {
+    var tracks = [{ uri: "spotify:track:4iV5W9uYEdYUVa79Axb7Rh" }];
+    var playlistId = '5ieJqeLJjjI8iJWaxeBLuK';
+    var options = { snapshot_id: "0wD+DKCUxiSR/WY8lF3fiCTb7Z8X4ifTUtqn8rO82O4Mvi5wsX8BsLj7IbIpLVM9" };
+    spotifyApi.removeTracksFromPlaylist(playlistId, tracks, options)
+        .then(function (data) {
+            console.log('Tracks removed from playlist!');
+        }, function (err) {
+            console.log('Something went wrong!', err);
+        });
+}
+
+export function createHelperPlaylist(spotifyApi) {
+    spotifyApi.createPlaylist('My playlist', { 'description': 'My description', 'public': true })
+        .then(function (data) {
+            console.log('Created playlist!');
+        }, function (err) {
+            console.log('Something went wrong!', err);
+        });
+}

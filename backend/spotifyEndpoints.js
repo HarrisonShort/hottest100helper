18aec5465704e32b50680930f600e713afa74c1d
HarrisonShort <work.harrisonshort@gmail.com>
2022-08-31 15:25:19 +1000

    CHANGE to use spotifyEndpoints file from server.js

--- backend/spotifyEndpoints.js
+++ backend/spotifyEndpoints.js
@@ -7,14 +7,43 @@ const credentials = {
 }
 
 const login = (req, res) => {
-    let spotifyApi = new SpotifyWebApi(credentials);
+    const spotifyApi = new SpotifyWebApi({
+        clientId: 'e46e02da24384042b7a9d4a7cab689df',
+        clientSecret: '3a45111efb444494b7b66f16a0259ddd',
+        redirectUri: 'http://localhost:3000/'
+    });
+
     const code = req.body.code;
 
-    spotifyApi.authorizationCodeGrant(code)
+    spotifyApi
+        .authorizationCodeGrant(code)
         .then((data) => {
             res.json({
                 accessToken: data.body.access_token,
-                refreshToken: data.body.refresh_token
+                refreshToken: data.body.refresh_token,
+                expiredIn: data.body.expires_in
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.sendStatus(400);
+        });
+}
+
+const refresh = (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    const spotifyApi = new SpotifyWebApi({
+        clientId: 'e46e02da24384042b7a9d4a7cab689df',
+        clientSecret: '3a45111efb444494b7b66f16a0259ddd',
+        redirectUri: 'http://localhost:3000/',
+        refreshToken: refreshToken
+    });
+
+    spotifyApi.refreshAccessToken()
+        .then((data) => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn
             });
         })
         .catch((error) => {
@@ -24,5 +53,6 @@ const login = (req, res) => {
 }
 
 module.exports = {
-    login
+    login,
+    refresh
 };

e7b2948edefe18e238289b4e1af98bbd10b293d0
HarrisonShort <work.harrisonshort@gmail.com>
2022-09-24 19:03:36 +1000

    ADD showing About component and preventing scrolling

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,29 +1,43 @@
-import React, { Link } from 'react'
+import React, { useState, useEffect } from 'react'
 import { loginUrl } from '../../spotifyConfig'
+import About from '../pages/About';
 import './Header.css';
 
 export default function Header(props) {
+    const [showAbout, setShowAbout] = useState(false);
+
     const signInJsx = <a href={loginUrl}>Sign in with Spotify!</a>
     const userNameJsx = <p className="user-name">{props.username}</p>;
     const imageJsx = <img src={props.image} alt="user" className="user-image"></img>
 
+    function openAboutOverlay() {
+        setShowAbout(!showAbout)
+    }
+
+    useEffect(() => {
+        document.body.style.overflow = showAbout ? 'hidden' : 'auto';
+    }, [showAbout])
+
     return (
-        <nav className="header">
-            <div className="header-container">
-                <div className="header-title">
-                    <p>Hottest 100 Helper</p>
+        <div>
+            <nav className="header">
+                <div className="header-container">
+                    <div className="header-title">
+                        <p>Hottest 100 Helper</p>
+                    </div>
+                    <div className="nav-links">
+                        <button onClick={() => openAboutOverlay()}>
+                            About
+                        </button>
+                    </div>
+                    <div className="user-details">
+                        {!props.username ? signInJsx : ""}
+                        {props.username ? userNameJsx : ""}
+                        {props.image ? imageJsx : ""}
+                    </div>
                 </div>
-                <div className="nav-links">
-                    <p className="header-item">
-                        About
-                    </p>
-                </div>
-                <div className="user-details">
-                    {!props.username ? signInJsx : ""}
-                    {props.username ? userNameJsx : ""}
-                    {props.image ? imageJsx : ""}
-                </div>
-            </div>
-        </nav>
+            </nav>
+            {showAbout ? <About /> : null}
+        </div>
     )
 };

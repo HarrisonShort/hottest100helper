ba9917d7e8900f7699362e23e0bd4b4452a9525b
HarrisonShort <work.harrisonshort@gmail.com>
2022-10-09 12:48:12 +1000

    ADD hiding the album column in the case that the window size is smaller than a certain const value (columnHideWidth)

--- client/src/components/spotify/SpotifyDataTable.js
+++ client/src/components/spotify/SpotifyDataTable.js
@@ -1,11 +1,15 @@
 import React, { useMemo, useState, useEffect } from 'react'
 import { useTable, useSortBy } from 'react-table';
+import { useWindowSize } from '../../customHooks/useWindowSize';
 import './SpotifyDataTable.css';
+
+const columnHideWidth = 500;
 
 export default function SpotifyDataTable(props) {
     const columns = useMemo(() => [...props.columns], [props.columns]);
     const data = useMemo(() => [...props.tracks], [props.tracks]);
     const [currentSort, setCurrentSort] = useState([]);
+    const windowSize = useWindowSize();
 
     const tableHooks = (hooks) => {
         if (!props.showShortlist) {
@@ -29,7 +33,8 @@ export default function SpotifyDataTable(props) {
         headerGroups,
         rows,
         prepareRow,
-        state: { sortBy }
+        state: { sortBy },
+        allColumns
     } = useTable({
         columns,
         data,
@@ -43,47 +48,84 @@ export default function SpotifyDataTable(props) {
         setCurrentSort(sortBy);
     }, [sortBy])
 
-    const noDataJsx = <p>{props.warningText}</p>;
-    const headersJsx = (headerGroups.map(headerGroup => (
-        <tr {...headerGroup.getHeaderGroupProps()}>
-            {
-                headerGroup.headers.map(column => (
-                    <th {...column.getHeaderProps(column.getSortByToggleProps())} id={column.id}>
-                        {column.render('Header')}
-                        {
-                            column.id === "shortlist" ? null : <span className="sort-icon">
-                                {column.isSorted ?
-                                    (column.isSortedDesc ? <img src={"../images/down_arrow.png"} alt="desc" /> : <img src={"../images/up_arrow.png"} alt="asc" />)
-                                    : <img src={"../images/both_arrow.png"} alt="both" />}
-                            </span>
-                        }
-                    </th>
-                ))
-            }
-        </tr>
-    )));
-    const rowsJsx = (rows.map((row, i) => {
-        prepareRow(row);
+    /**
+     * In order to save space on smaller screens, we hide the album column in the case
+     * that the screen is smaller than a certain size.
+     */
+    const toggleAlbumColumn = () => {
+        let albumColumnIndex = allColumns.findIndex((column) => column.id === "album");
+        let isVisible = allColumns[albumColumnIndex].isVisible;
+
+        if ((windowSize.width > columnHideWidth && !isVisible)
+            || (windowSize.width < columnHideWidth && isVisible)) {
+            allColumns[albumColumnIndex].toggleHidden();
+        }
+    }
+
+    const drawColumnHeaderGroup = (headerGroup) => {
+        toggleAlbumColumn();
+
         return (
-            <tr {...row.getRowProps()}>
+            <tr {...headerGroup.getHeaderGroupProps()}>
                 {
-                    row.cells.map(cell => {
-                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-                    })
+                    headerGroup.headers.map(column => (
+                        <th {...column.getHeaderProps(column.getSortByToggleProps())} id={column.id}>
+                            {column.render('Header')}
+                            {drawSortArrows(column)}
+                        </th>
+                    ))
                 }
             </tr>
-        );
-    }));
+        )
+    }
+
+    const drawSortArrows = (column) => {
+        return (
+            column.id === "shortlist" ? null : <span className="sort-icon">
+                {
+                    column.isSorted ?
+                        (
+                            column.isSortedDesc ?
+                                <img src={"../images/down_arrow.png"} alt="desc" />
+                                : <img src={"../images/up_arrow.png"} alt="asc" />
+                        )
+                        : <img src={"../images/both_arrow.png"} alt="both" />
+                }
+            </span>
+        )
+    }
+
+    const drawDataRows = () => {
+        return (
+            rows.map((row, i) => {
+                prepareRow(row);
+                return (
+                    <tr {...row.getRowProps()}>
+                        {
+                            row.cells.map(cell => {
+                                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                            })
+                        }
+                    </tr>
+                );
+            })
+        )
+    }
 
     return (
-        <div>
-            {data.length === 0 ? noDataJsx :
+        <div className="table-container">
+            {data.length === 0 ?
+                <p>{props.warningText}</p> :
                 <table {...getTableProps()}>
                     <thead>
-                        {headersJsx}
+                        {
+                            headerGroups.map(headerGroup => (
+                                drawColumnHeaderGroup(headerGroup)
+                            ))
+                        }
                     </thead>
                     <tbody {...getTableBodyProps()}>
-                        {rowsJsx}
+                        {drawDataRows()}
                     </tbody>
                 </table>
             }

b98e379a7b47f295dac1a90a84a464195def2e30
HarrisonShort <work.harrisonshort@gmail.com>
2022-10-11 19:30:07 +1000

    CHANGE to use environment variables

--- backend/spotifyEndpoints.js
+++ backend/spotifyEndpoints.js
@@ -1,10 +1,11 @@
 const SpotifyWebApi = require('spotify-web-api-node');
+require('dotenv').config({ path: './.env' });
 
 const login = (req, res) => {
     const spotifyApi = new SpotifyWebApi({
-        clientId: 'e46e02da24384042b7a9d4a7cab689df',
-        clientSecret: '3a45111efb444494b7b66f16a0259ddd',
-        redirectUri: 'http://localhost:3000/'
+        clientId: process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_CLIENT_ID : process.env.DEVELOPMENT_CLIENT_ID,
+        clientSecret: process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_CLIENT_SECRET : process.env.DEVELOPMENT_CLIENT_SECRET,
+        redirectUri: process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_REDIRECT_URI : process.env.DEVELOPMENT_REDIRECT_URI
     });
 
     const code = req.body.code;
